3dd0e65c43786c744fcf919599ef048caccf32b4
Chaima <1925046@alu.murciaeduca.es>
2025-01-27 22:42:34 +0100

    parece que ya esta

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,7 +5,6 @@ import org.apache.logging.log4j.Logger;
 
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.util.Scanner;
 import java.util.regex.Matcher;
